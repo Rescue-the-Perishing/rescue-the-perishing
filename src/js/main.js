91837f1cdb93370dfdce705cd152d2e3140b2432
Rescue-the-Perishing <Rescue-the-Perishing@users.noreply.github.com>
2014-06-18 14:31:16 -0400

    Added translation framework and Spanish translation

--- src/js/main.js
+++ src/js/main.js
@@ -85,7 +85,7 @@ function calculateDeathsPerPeriod(period, type) {
 
         //Obtain Death Rate form element
         deathRate = document.getElementById("death-rate").value,
-        
+
         //Obtain Lost Rate
         lostRate = calculateLostRate(),
 
@@ -160,21 +160,23 @@ function calculateCounterRate() {
     return forTimeOut;
 }
 
-function startTime() {
+function startTime(lang) {
 /*Configure and display the time elapsed timer (i.e, jquery.countdown)*/
 
     "use strict";
 
     $(function () {
+
         //set variable used by jquery.countdown to the current time
         var austDay = new Date();
 
         //configure jquery.countdown to start counting up (i.e., since) from the variable defined above
         //make the display format pretty
-        $('#defaultCountdown').countdown({since: austDay, format: 'yodhmS', layout: '{d<}{dn} {dl}, {d>}{h<}{hn} {hl}, {h>}{m<}{mn} {ml}, {m>}{sn} {sl}'});
+        $('#defaultCountdown').countdown($.extend({since: austDay, format: 'yodhmS', layout: '{d<}{dn} {dl}, {d>}{h<}{hn} {hl}, {h>}{m<}{mn} {ml}, {m>}{sn} {sl}'},$.countdown.regionalOptions[lang]));
+
     });
-}   
-    
+}
+
 /*Display the mortality rate count-up counter, inserting commas as necessary*/
 /*By George Chiang. (JK's ultimate JavaScript tutorial and free JavaScripts site!)
 http://www.javascriptkit.com
@@ -216,7 +218,7 @@ function insertDefaults() {
     $("#death-rate").val("7.9");
     $("#reached-rate").val("7.9");
     $("#location").val("the world");
-    
+
     /*
     References:
     - Population: estimated world population at midyear 2013 as reported at http://www.census.gov/population/international/data/idb/region.php?N=%20Results%20&T=6&A=aggregate&RT=0&Y=2013&R=1&C=
@@ -232,26 +234,100 @@ function initiateDisplay() {
     "use strict";
 
     //Obtain output-type form element
-    var outputType = document.getElementById("output-type").value,
+    var period = document.getElementById("output-type").value,
 
     //Obtain summary type (total/everyone or lost)
-        summaryType = $('input[name="summary-type"]:checked').val(),
+        type = $('input[name="summary-type"]:checked').val(),
 
     //obtain the number of deaths for the specified period and summary type
-        deathsPerPeriod = calculateDeathsPerPeriod(outputType,summaryType),
+        deathsPerPeriod = calculateDeathsPerPeriod(period, type),
 
     //Obtain Location form element
         location = document.getElementById("location").value,
-    
-    //Set summary type text to empty value
-        summaryTypeText = "";
+
+    //Create language variable to be used to localize jquery.countdown (an empty string defaults to English)
+        lang = "",
+
+    //Obtain language form element
+        language = document.getElementById("language").value,
+
+    //Initiate variables to hold translations
+        rtpLanguageCode = "",
+        rtpApproximately = "Approximately",
+        rtpPeople = "people in",
+        rtpLost = "<em>lost</em> people in",
+        rtpEternity = "die and enter eternity every",
+        rtpBegan = "Since",
+        rtpAgo = "ago, the following number of persons in",
+        rtpEnter = "have died and entered eternity <em>without Christ</em>",
+        rtpSecond = "Second",
+        rtpMinute = "Minute",
+        rtpHour = "Hour",
+        rtpDay = "Day",
+        rtpWeek = "Week",
+        rtpYear = "Year";
+
+    /*Show different translations based on selected language*/
+    if (language === "Spanish") {
+        rtpLanguageCode = "es";
+        rtpApproximately = "Aproximadamente";
+        rtpPeople = "personas en";
+        rtpLost = "personas <em>perdidas</em> en",
+        rtpEternity = "mueren y entran a la eternidad cada";
+        rtpBegan = "Desde";
+        rtpAgo = "atrás, el siguiente número de personas en";
+        rtpEnter = "han muerto y entrado a la eternidad <em>sin Cristo</em>";
+        rtpSecond = "Segundo";
+        rtpMinute = "Minuto";
+        rtpHour = "Hora";
+        rtpDay = "Día";
+        rtpWeek = "Semana";
+        rtpYear = "Año";
+        }
 
     //Change summary type text if lost is selected
-    if (summaryType === "Lost") {
-        summaryTypeText = "<em>lost</em> ";
-    }   
+    if (type === "Lost") {
+        rtpPeople = rtpLost;
+    }
+    
+    //switch number format based on provided language code
+    numeral.language(rtpLanguageCode);
 
-    /*reformat the output numbers*/
+    //set language for jquery.countdown based on provided language code
+    lang = rtpLanguageCode;
+
+    //Set period text based on user's selection (Second, Minute, Hour, Day, Week, or Year) and corresponding translation
+    switch (period) {
+    case "Second":
+        period = rtpSecond;
+        break;
+    case "Minute":
+        period = rtpMinute;
+        break;
+    case "Hour":
+        period = rtpHour;
+        break;
+    case "Day":
+        period = rtpDay;
+        break;
+    case "Week":
+        period = rtpWeek;
+        break;
+    case "Year":
+        period = rtpYear;
+        break;
+    default:
+        //return error message if receive non-expected input
+        alert('Please provide a string of Second, Minute, Hour, Day, Week, or Year as the period');
+    }
+
+    //Insert the corresponding summary-type text into page via jQuery library
+    $('.rtp-approximately').html(rtpApproximately);
+    $('.rtp-people').html(rtpPeople);
+    $('.rtp-eternity').html(rtpEternity);
+    $('.rtp-began').html(rtpBegan);
+    $('.rtp-ago').html(rtpAgo);
+    $('.rtp-enter').html(rtpEnter);
 
     //set numeral-js default format to rounded number with comma (e.g., 1,000)
     numeral.defaultFormat('0,0');
@@ -260,7 +336,7 @@ function initiateDisplay() {
     deathsPerPeriod = numeral(deathsPerPeriod).format();
 
     //Insert the output-type period (day, week, etc.) into page via jQuery library
-    $('.period-holder').html(outputType.toLowerCase());
+    $('.period-holder').html(period.toLowerCase());
 
     //Insert the calculated deaths per day into page via jQuery library
     $('#deathsPerPeriod').html(deathsPerPeriod);
@@ -269,17 +345,17 @@ function initiateDisplay() {
     $('.location-holder').html(location);
 
     //Insert the corresponding summary-type text into page via jQuery library
-    $('.summary-type-holder').html(summaryTypeText);    
-    
-    //start the time elapsed timer
-    startTime();
+    $('.summary-type-holder').html(rtpPeople);
+
+    //start the time elapsed timer, passing in selected language
+    startTime(lang);
 
     //start the mortality counter
     display();
 
     //close the configuration pop-up
     $.magnificPopup.close();
-    
+
     //hide the configuration link text
     $("p.configure").hide();
 }
