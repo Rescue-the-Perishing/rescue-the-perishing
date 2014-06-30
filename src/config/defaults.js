e087630a7e490612affcc35b6416650c3a5a9c59
Rescue-the-Perishing <Rescue-the-Perishing@users.noreply.github.com>
2014-06-30 14:41:42 -0400

    Revised defaults

--- src/config/defaults.js
+++ src/config/defaults.js
@@ -1,9 +1,9 @@
 var defaults =
     {
-        "deathRate" : "7.9",
-        "location" : "the world",
         "population" : "7098495231",
+        "deathRate" : "7.89",
         "reachedRate" : "7.9",
+        "location" : "the world",
         "language" : "English", //Supported languages are "English", "Spanish", and "Portuguese"
         "summarize_output" : "Day", //Supported values are "Second", Minute", "Hour", "Day", "Week", and "Year"
         "report_summary_total" : "Everyone" //Supported values are "Everyone" and "Lost"
