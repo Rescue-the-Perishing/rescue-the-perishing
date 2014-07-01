8e657d5983a4e1ab4e1da5e27f4b882f851d3086
Rescue-the-Perishing <Rescue-the-Perishing@users.noreply.github.com>
2014-06-30 21:39:00 -0400

    Updated site with 0.7.0 release

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
       midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
     });
 
-//Show/hid advanced configuration options when clicked via jQuery library
+//Show/hide advanced configuration options when clicked via jQuery library
     $( ".show-advanced-options" ).click(function() {
         $( "#advanced-options" ).toggle( "fast", function() {
         });
@@ -15,9 +15,14 @@ function calculateLostRate() {
     "use strict";
     //Obtain reached-rate form element
     var reachedRate = document.getElementById("reached-rate").value,
+        lostRate = "";
 
-        //Calculate lost rate
-        lostRate = (100 - reachedRate) / 100;
+    if (reachedRate === "") {
+        reachedRate = defaults.reachedRate;
+    }
+
+    //Calculate lost rate
+    lostRate = (100 - reachedRate) / 100;
 
     return lostRate;
 }
@@ -74,7 +79,7 @@ function calculateDeathsPerPeriod(period, type) {
 "Year"
 
 *The 'type' argument is expected to be one of the following strings:
-"Total"
+"Everyone"
 "Lost"
 */
 
@@ -85,20 +90,27 @@ function calculateDeathsPerPeriod(period, type) {
 
         //Obtain Death Rate form element
         deathRate = document.getElementById("death-rate").value,
-        
-        //Obtain Lost Rate
+
         lostRate = calculateLostRate(),
+        deathRatePercentage = "",
+        deathsYear = "",
+        deathsPerSecond = "";
 
-        //Convert crude death rate to percentage
-        deathRatePercentage = deathRate / 1000,
+    if (population === "" && deathRate === "") {
+        population = defaults.population;
+        deathRate = defaults.deathRate;
+    }
 
-        //Calculate deaths a year
-        deathsYear = deathRatePercentage * population,
+    //Convert crude death rate to percentage
+    deathRatePercentage = deathRate / 1000;
 
-        //Calculate deaths per second
-        deathsPerSecond = deathsYear / secondsPerPeriod("Year");
+    //Calculate deaths a year
+    deathsYear = deathRatePercentage * population,
 
-    if (type === "Total") {
+    //Calculate deaths per second
+    deathsPerSecond = deathsYear / secondsPerPeriod("Year");
+
+    if (type === "Everyone") {
         switch (period) {
         case "Second":
             return deathsPerSecond;
@@ -126,22 +138,22 @@ function calculateDeathsPerPeriod(period, type) {
     if (type === "Lost") {
         switch (period) {
         case "Second":
-            var deathsPerSecondLost = calculateDeathsPerPeriod("Second", "Total") * lostRate;
+            var deathsPerSecondLost = calculateDeathsPerPeriod("Second", "Everyone") * lostRate;
             return deathsPerSecondLost;
         case "Minute":
-            var deathsPerMinuteLost = calculateDeathsPerPeriod("Minute", "Total") * lostRate;
+            var deathsPerMinuteLost = calculateDeathsPerPeriod("Minute", "Everyone") * lostRate;
             return deathsPerMinuteLost;
         case "Hour":
-            var deathsPerHourLost = calculateDeathsPerPeriod("Hour", "Total") * lostRate;
+            var deathsPerHourLost = calculateDeathsPerPeriod("Hour", "Everyone") * lostRate;
             return deathsPerHourLost;
         case "Day":
-            var deathsPerDayLost = calculateDeathsPerPeriod("Day", "Total") * lostRate;
+            var deathsPerDayLost = calculateDeathsPerPeriod("Day", "Everyone") * lostRate;
             return deathsPerDayLost;
         case "Week":
-            var deathsPerWeekLost = calculateDeathsPerPeriod("Week", "Total") * lostRate;
+            var deathsPerWeekLost = calculateDeathsPerPeriod("Week", "Everyone") * lostRate;
             return deathsPerWeekLost;
         case "Year":
-            var deathsPerYearLost = calculateDeathsPerPeriod("Year", "Total") * lostRate;
+            var deathsPerYearLost = calculateDeathsPerPeriod("Year", "Everyone") * lostRate;
             return deathsPerYearLost;
         default:
             //return error message if receive non-expected input
@@ -160,21 +172,23 @@ function calculateCounterRate() {
     return forTimeOut;
 }
 
-function startTime() {
+function startTime(languageCode) {
 /*Configure and display the time elapsed timer (i.e, jquery.countdown)*/
 
     "use strict";
 
     $(function () {
+
         //set variable used by jquery.countdown to the current time
         var austDay = new Date();
 
         //configure jquery.countdown to start counting up (i.e., since) from the variable defined above
         //make the display format pretty
-        $('#defaultCountdown').countdown({since: austDay, format: 'yodhmS', layout: '{d<}{dn} {dl}, {d>}{h<}{hn} {hl}, {h>}{m<}{mn} {ml}, {m>}{sn} {sl}'});
+        $('#defaultCountdown').countdown($.extend({since: austDay, format: 'yodhmS', layout: '{d<}{dn} {dl}, {d>}{h<}{hn} {hl}, {h>}{m<}{mn} {ml}, {m>}{sn} {sl}'},$.countdown.regionalOptions[languageCode]));
+
     });
-}   
-    
+}
+
 /*Display the mortality rate count-up counter, inserting commas as necessary*/
 /*By George Chiang. (JK's ultimate JavaScript tutorial and free JavaScripts site!)
 http://www.javascriptkit.com
@@ -212,74 +226,301 @@ function display() {
 
 function insertDefaults() {
     //Insert values into configuration form based on world statistics
-    $("#population").val("7098495231");
-    $("#death-rate").val("7.9");
-    $("#reached-rate").val("7.9");
-    $("#location").val("the world");
-    
-    /*
-    References:
-    - Population: estimated world population at midyear 2013 as reported at http://www.census.gov/population/international/data/idb/region.php?N=%20Results%20&T=6&A=aggregate&RT=0&Y=2013&R=1&C=
-    - Crude Death Rate: Based on estimated death rate (per 1,000 population) for the world as reported at https://www.cia.gov/library/publications/the-world-factbook/geos/xx.html
-    - Reached Rate: Based on population percentage of Evangelicals at http://www.operationworld.org/wrld
-    */
-
+    $("#population").val(defaults.population);
+    $("#death-rate").val(defaults.deathRate);
+    $("#reached-rate").val(defaults.reachedRate);
+    $("#location").val(defaults.location);
 }
 
-function initiateDisplay() {
-/*Initiate and display everything when start is invoked by the user*/
+function obtainLanguageCode(language) {
+/*Return the language code based on user selected language*/
+
+    "use strict";
+
+    var languageCode = "";
+
+    //language codes used must match one of the provided translation files within src/js/vendor/countdown and src/js/vendor/numeral-js/languages
+    switch (language) {
+    case "Spanish":
+        languageCode = "es";
+        break;
+    case "Portuguese":
+        languageCode = "pt-br";
+        break;
+    default:
+        //default to blank if none of the above match (a blank language code will use English)
+        languageCode = "";
+    }
+
+    return languageCode;
+}
+
+function setWording(language) {
+/*Update the wording based on the provided language, defaults to English if the language provided is not supported*/
 
     "use strict";
 
     //Obtain output-type form element
-    var outputType = document.getElementById("output-type").value,
+    var period = document.getElementById("output-type").value,
 
-    //Obtain summary type (total/everyone or lost)
-        summaryType = $('input[name="summary-type"]:checked').val(),
+    //Obtain summary type (everyone or lost)
+        type = $('input[name="summary-type"]:checked').val(),
 
     //obtain the number of deaths for the specified period and summary type
-        deathsPerPeriod = calculateDeathsPerPeriod(outputType,summaryType),
+        deathsPerPeriod = calculateDeathsPerPeriod(period, type),
 
     //Obtain Location form element
         location = document.getElementById("location").value,
-    
-    //Set summary type text to empty value
-        summaryTypeText = "";
 
-    //Change summary type text if lost is selected
-    if (summaryType === "Lost") {
-        summaryTypeText = "<em>lost</em> ";
-    }   
+    //Initiate a variable object to hold translations
+        phrases =
+            {
+                rtpApproximately : "",
+                rtpPeople : "",
+                rtpLost : "",
+                rtpEternity : "",
+                rtpBegan : "",
+                rtpAgo : "",
+                rtpDownload : "",
+                rtpSecond : "",
+                rtpSeconds : "",
+                rtpMinute : "",
+                rtpHour : "",
+                rtpDay : "",
+                rtpWeek : "",
+                rtpYear : "",
+                rtpDefaultCountdown : ""
+            };
 
-    /*reformat the output numbers*/
+    //Update phrases variable to match the selected translation
+    switch (language) {
+    case "Spanish":
+        phrases.rtpApproximately = spanish.rtp_Approximately;
+        phrases.rtpPeople = spanish.rtp_People;
+        phrases.rtpLost = spanish.rtp_Lost;
+        phrases.rtpEternity = spanish.rtp_Eternity;
+        phrases.rtpBegan = spanish.rtp_Began;
+        phrases.rtpAgo = spanish.rtp_Ago;
+        phrases.rtpDownload = spanish.rtp_Download;
+        phrases.rtpSecond = spanish.rtp_Second;
+        phrases.rtpSeconds = spanish.rtp_Seconds;
+        phrases.rtpMinute = spanish.rtp_Minute;
+        phrases.rtpHour = spanish.rtp_Hour;
+        phrases.rtpDay = spanish.rtp_Day;
+        phrases.rtpWeek = spanish.rtp_Week;
+        phrases.rtpYear = spanish.rtp_Year;
+        break;
+    case "Portuguese":
+        phrases.rtpApproximately = portuguese.rtp_Approximately;
+        phrases.rtpPeople = portuguese.rtp_People;
+        phrases.rtpLost = portuguese.rtp_Lost;
+        phrases.rtpEternity = portuguese.rtp_Eternity;
+        phrases.rtpBegan = portuguese.rtp_Began;
+        phrases.rtpAgo = portuguese.rtp_Ago;
+        phrases.rtpDownload = portuguese.rtp_Download;
+        phrases.rtpSecond = portuguese.rtp_Second;
+        phrases.rtpSeconds = portuguese.rtp_Seconds;
+        phrases.rtpMinute = portuguese.rtp_Minute;
+        phrases.rtpHour = portuguese.rtp_Hour;
+        phrases.rtpDay = portuguese.rtp_Day;
+        phrases.rtpWeek = portuguese.rtp_Week;
+        phrases.rtpYear = portuguese.rtp_Year;
+        break;
+    default:
+        phrases.rtpApproximately = english.rtp_Approximately;
+        phrases.rtpPeople = english.rtp_People;
+        phrases.rtpLost = english.rtp_Lost;
+        phrases.rtpEternity = english.rtp_Eternity;
+        phrases.rtpBegan = english.rtp_Began;
+        phrases.rtpAgo = english.rtp_Ago;
+        phrases.rtpDownload = english.rtp_Download;
+        phrases.rtpSecond = english.rtp_Second;
+        phrases.rtpSeconds = english.rtp_Seconds;
+        phrases.rtpMinute = english.rtp_Minute;
+        phrases.rtpHour = english.rtp_Hour;
+        phrases.rtpDay = english.rtp_Day;
+        phrases.rtpWeek = english.rtp_Week;
+        phrases.rtpYear = english.rtp_Year;
+    }
 
-    //set numeral-js default format to rounded number with comma (e.g., 1,000)
-    numeral.defaultFormat('0,0');
+    //Change people to lost phrase if lost is selected
+    if (type === "Lost") {
+        phrases.rtpPeople = phrases.rtpLost;
+   }
+
+    //change 0 seconds place-holder wording based on language code
+    phrases.rtpDefaultCountdown = "0 " + phrases.rtpSeconds;
+
+    //set language for numeral-js based on provided language code
+    numeral.language(obtainLanguageCode(language));
+
+    //Set period text based on user's selection (Second, Minute, Hour, Day, Week, or Year) and corresponding translation
+    switch (period) {
+    case "Second":
+        period = phrases.rtpSecond;
+        break;
+    case "Minute":
+        period = phrases.rtpMinute;
+        break;
+    case "Hour":
+        period = phrases.rtpHour;
+        break;
+    case "Day":
+        period = phrases.rtpDay;
+        break;
+    case "Week":
+        period = phrases.rtpWeek;
+        break;
+    case "Year":
+        period = phrases.rtpYear;
+        break;
+    default:
+        //return error message if receive non-expected input
+        alert('Please provide a string of Second, Minute, Hour, Day, Week, or Year as the period');
+    }
 
     //format the deaths per period using the default format
     deathsPerPeriod = numeral(deathsPerPeriod).format();
 
-    //Insert the output-type period (day, week, etc.) into page via jQuery library
-    $('.period-holder').html(outputType.toLowerCase());
-
-    //Insert the calculated deaths per day into page via jQuery library
+    //Insert the corresponding text into page via jQuery library
+    $('.rtp-approximately').html(phrases.rtpApproximately);
+    $('.rtp-people').html(phrases.rtpPeople);
+    $('.rtp-eternity').html(phrases.rtpEternity);
+    $('.rtp-began').html(phrases.rtpBegan);
+    $('.rtp-ago').html(phrases.rtpAgo);
+    $('#defaultCountdown').html(phrases.rtpDefaultCountdown);
+    $('.rtp-download').html(phrases.rtpDownload);
+    $('.summary-type-holder').html(phrases.rtpPeople);
+    $('.period-holder').html(period.toLowerCase());
     $('#deathsPerPeriod').html(deathsPerPeriod);
-
-    //Insert the location into page via jQuery library
     $('.location-holder').html(location);
 
-    //Insert the corresponding summary-type text into page via jQuery library
-    $('.summary-type-holder').html(summaryTypeText);    
-    
-    //start the time elapsed timer
-    startTime();
+}
+
+function ok() {
+/*Update the wording and close the pop-up when ok button is clicked*/
+
+    "use strict";
+
+    //Obtain language form element
+    var language = document.getElementById("language").value;
+
+    //Update the wording based on selected settings
+    setWording(language);
+
+    //close the configuration pop-up
+    $.magnificPopup.close();
+
+}
+
+function initiateDisplay() {
+/*Start counter and timer when start is invoked by the user, using defaults if user does not configure values before starting*/
+
+    "use strict";
+
+    //Obtain necessary form elements
+    var population = document.getElementById("population").value,
+        deathRate = document.getElementById("death-rate").value,
+        location = document.getElementById("location").value,
+        reachedRate = document.getElementById("reached-rate").value,
+        language = document.getElementById("language").value;
+
+    //if form is blank, insert and accept default values before running
+    if (population === "" && deathRate === "" && location === "" && reachedRate === "") {
+        $("#population").val(defaults.population);
+        $("#death-rate").val(defaults.deathRate);
+        $("#reached-rate").val(defaults.reachedRate);
+        $("#location").val(defaults.location);
+        ok();
+    }
+
+    //start the time elapsed timer, passing in selected language
+    startTime(obtainLanguageCode(language));
 
     //start the mortality counter
     display();
 
-    //close the configuration pop-up
-    $.magnificPopup.close();
-    
     //hide the configuration link text
     $("p.configure").hide();
 }
+
+$(document).ready(function () {
+/*insert defaults when the page is loaded*/
+
+    "use strict";
+
+    //Obtain default for 'Summarize Output By'
+    var period = defaults.summarize_output,
+
+    //Obtain default for 'Report Summary Total For'
+        type = defaults.report_summary_total,
+
+    //Set deaths per period to value calculated based on defaults
+        deathsPerPeriod = calculateDeathsPerPeriod(period, type);
+
+    //format the deaths per period
+    deathsPerPeriod = numeral(deathsPerPeriod).format();
+
+    //change dropdown form selection to match the language set in the defaults file
+    switch (defaults.language) {
+    case "English":
+        $("#language option[value='English']").attr("selected", "selected");
+        break;
+    case "Spanish":
+        $("#language option[value='Spanish']").attr("selected", "selected");
+        break;
+    case "Portuguese":
+        $("#language option[value='Portuguese']").attr("selected", "selected");
+        break;
+    default:
+        alert("The default language is set to " + defaults.language + ", which is not currently supported option. Please set a supported language in the config/defaults.js file");
+    }
+
+    //change dropdown form selection to match the summarize_output set in the defaults file
+    switch (defaults.summarize_output) {
+    case "Day":
+        $("#output-type option[value='Day']").attr("selected", "selected");
+        break;
+    case "Second":
+        $("#output-type option[value='Second']").attr("selected", "selected");
+        break;
+    case "Minute":
+        $("#output-type option[value='Minute']").attr("selected", "selected");
+        break;
+    case "Hour":
+        $("#output-type option[value='Hour']").attr("selected", "selected");
+        break;
+    case "Week":
+        $("#output-type option[value='Week']").attr("selected", "selected");
+        break;
+    case "Year":
+        $("#output-type option[value='Year']").attr("selected", "selected");
+        break;
+    default:
+        alert('The default "Summarize Output By" is set to "' + defaults.summarize_output + '". Please set a supported value in the config/defaults.js file');
+    }
+
+    //change radio button form to match the report_summary_total set in the defaults file
+    switch (defaults.report_summary_total) {
+    case "Everyone":
+        $('input:radio[name="summary-type"]').filter('[value="Everyone"]').prop('checked', true);
+        break;
+    case "Lost":
+        $('input:radio[name="summary-type"]').filter('[value="Lost"]').prop('checked', true);
+        break;
+    default:
+        alert('The default "Report Summary Total For" is set to "' + defaults.report_summary_total + '". Please set a supported value in the config/defaults.js file');
+    }
+
+    //Insert items not set by the setWording function
+    $('#deathsPerPeriod').html(deathsPerPeriod);
+    $('.period-holder').html(period.toLowerCase());
+    
+    //Ensure the real-time mortality counter returns to 0 on page reload (without doing this IE retains old counter value)
+    $('#counter').val("0");
+
+    //make page wording show the selected translation
+    setWording(defaults.language);
+
+    $('.location-holder').html(defaults.location);
+});
